f314a559f35d1188caa0d35f4faaf1e565af6001
Sidharthreddyvangoor <93504292+Sidharthreddyvangoor@users.noreply.github.com>
2022-11-29 06:01:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
+" > ~/.streamlit/pyproject.toml
 echo "\
 [server]\n\
 headless = true\n\
